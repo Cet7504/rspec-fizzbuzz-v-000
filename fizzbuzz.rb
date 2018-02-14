98e1b87b0b17a033c9bb520d7089a28fd6b8e036
Christian Timberlake <timberlakece1@my.palmbeachstate.edu>
2018-02-14 22:14:45 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,11 @@
 require 'pry'
 
-def fizzbuzz(fizz_3)
+def fizzbuzz(fizz_3, fizz_5, fizz_15,)
   if fizz_3 % 3 == 0
+    if fizz_5 % 5 == 0
+      if fizz_15 % (3,5) == 0
+        "FizzBuzz"
+      "Buzz"
     "Fizz"
   end
 end
